0873974ba22aa5e7b2701f408d77a677f1c27fe9
renan augusto <renan.augusto09@gmail.com>
2022-09-29 22:07:01 -0300

    Fragmento produto adicionado fragmento album configurado para tela de usuário

--- app/src/main/java/br/senac/pi/fragments/AlbumsFragment.kt
+++ app/src/main/java/br/senac/pi/fragments/AlbumsFragment.kt
@@ -14,6 +14,14 @@ class AlbumsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentAlbumsBinding.inflate(inflater)
 
+        binding.imageSobre.setOnClickListener {
+            val frag = ArtistsFragment()
+            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.container, frag)?.commit()
+
+            true
+        }
+
+
         return binding.root
     }
 
