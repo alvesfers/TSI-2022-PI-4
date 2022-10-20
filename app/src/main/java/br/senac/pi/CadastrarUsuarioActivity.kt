a2d29b8a650acd7773df9e64d9d1d27ba876e4b0
renan augusto <renan.augusto09@gmail.com>
2022-10-20 20:40:22 -0300

    Tela de cadastro de usuário adicionada só design

---
+++ app/src/main/java/br/senac/pi/CadastrarUsuarioActivity.kt
@@ -0,0 +1,11 @@
+package br.senac.pi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastrarUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastrar_usuario)
+    }
+}
